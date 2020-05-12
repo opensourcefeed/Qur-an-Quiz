7986bb86a3a5286eab70712b7ca2946dcbe76344
Filmbiopsy <filmbiopsy@gmail.com>
2020-05-12 20:05:12 +0530

    Update broken static links

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,5 +1,5 @@
 export default {
-  QUESTION_COUNT: 2,
+  QUESTION_COUNT: 10,
   APP_STORAGE_KEY: 'filmbiopsy',
   REMOTE: 'http://www.opensourcefeed.org/Qur-an-Quiz/data/',
   REMOTE_DATA: 'https://filmbiopsy1.gitlab.io/mobile-app/',
